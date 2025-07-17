5e6b741ce03211e953a7ac57d65f3e73951f6682
Randi Syuja <syuja131202@gmail.com>
2025-07-17 23:12:15 +0700

    create an virtual environtment variable

--- constants.py
+++ constants.py
@@ -3,4 +3,4 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 LOCK_IMG_PATH = os.path.join(BASE_DIR, "img/lock.png")
-DATA_FILE = os.path.join(BASE_DIR, "database.json")
+DATA_FILE = os.path.join(BASE_DIR, "data/database.json")
